3bb0574d9eeccd66efb48155e349f20f7a09d023
Felipe Olaya Ospina <folaya@prevalentware.com>
2021-05-31 09:21:08 -0500

    dev

--- pages/developers/index.tsx
+++ pages/developers/index.tsx
@@ -1,9 +1,83 @@
 const index = () => {
-    return (
-        <>
-            
-        </>
-    )
-}
+  return (
+    <div>
+      <section className=' text-gray-200 bg-gray-900'>
+        <div className='max-w-6xl mx-auto px-5 py-24 '>
+          <div className='flex flex-wrap w-full mb-10S flex-col items-center text-center'>
+            <h1 className=' title-font mb-2 text-4xl font-extrabold leading-10 tracking-tight text-left sm:text-5xl sm:leading-none md:text-6xl'>
+              {' '}
+              DREAM TEAM
+            </h1>
+            <p className='lg:w-1/2 w-full leading-relaxed text-base'>Este es el Dream team! :v</p>
+          </div>
+        </div>
+        <div className='flex flex-wrap mx-4'>
+          <div className='xl:w-1/3 md:w-1/2 p-4'>
+            <div className='border border-gray-300 p-6 rounded-lg'>
+              <div className='w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4'>
+                <svg
+                  className=' fill-current h-6 w-6'
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 384 512'
+                >
+                  <path d='M0 32l34.9 395.8L191.5 480l157.6-52.2L384 32H0zm308.2 127.9H124.4l4.1 49.4h175.6l-13.6 148.4-97.9 27v.3h-1.1l-98.7-27.3-6-75.8h47.7L138 320l53.5 14.5 53.7-14.5 6-62.2H84.3L71.5 112.2h241.1l-4.4 47.7z' />
+                </svg>
+              </div>
+              <h2 className='text-lg  font-medium title-font mb-2'>Sebastian Arboleda Botero</h2>
+            </div>
+          </div>
 
-export default index
+          <div className='xl:w-1/3 md:w-1/2 p-4'>
+            <div className='border border-gray-300 p-6 rounded-lg'>
+            <div className='w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4'>
+                <svg
+                  className=' fill-current h-6 w-6 '
+                  viewBox='0 0 24 24'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <title>CSS3 icon</title>
+                  <path d='M1.5 0h21l-1.91 21.563L11.977 24l-8.565-2.438L1.5 0zm17.09 4.413L5.41 4.41l.213 2.622 10.125.002-.255 2.716h-6.64l.24 2.573h6.182l-.366 3.523-2.91.804-2.956-.81-.188-2.11h-2.61l.29 3.855L12 19.288l5.373-1.53L18.59 4.414z' />
+                </svg>
+              </div>
+              <h2 className='text-lg  font-medium title-font mb-2'>
+                Jose David Henao Ocampo
+              </h2>
+            </div>
+          </div>
+          <div className='xl:w-1/3 md:w-1/2 p-4'>
+            <div className='border border-gray-300 p-6 rounded-lg'>
+              <div className='w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4'>
+                <svg
+                  className=' fill-current h-6 w-6 '
+                  viewBox='0 0 24 24'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <title>CSS3 icon</title>
+                  <path d='M1.5 0h21l-1.91 21.563L11.977 24l-8.565-2.438L1.5 0zm17.09 4.413L5.41 4.41l.213 2.622 10.125.002-.255 2.716h-6.64l.24 2.573h6.182l-.366 3.523-2.91.804-2.956-.81-.188-2.11h-2.61l.29 3.855L12 19.288l5.373-1.53L18.59 4.414z' />
+                </svg>
+              </div>
+              <h2 className='text-lg  font-medium title-font mb-2'>Anthony García Moncada</h2>
+            </div>
+          </div>
+          <div className='xl:w-1/3 md:w-1/2 p-4'>
+            <div className='border border-gray-300 p-6 rounded-lg'>
+              <div className='w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4'>
+                <svg
+                  className=' fill-current h-6 w-6 '
+                  viewBox='0 0 24 24'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <title>CSS3 icon</title>
+                  <path d='M1.5 0h21l-1.91 21.563L11.977 24l-8.565-2.438L1.5 0zm17.09 4.413L5.41 4.41l.213 2.622 10.125.002-.255 2.716h-6.64l.24 2.573h6.182l-.366 3.523-2.91.804-2.956-.81-.188-2.11h-2.61l.29 3.855L12 19.288l5.373-1.53L18.59 4.414z' />
+                </svg>
+              </div>
+              <h2 className='text-lg  font-medium title-font mb-2'>Felipe Olaya Ospina</h2>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default index;
